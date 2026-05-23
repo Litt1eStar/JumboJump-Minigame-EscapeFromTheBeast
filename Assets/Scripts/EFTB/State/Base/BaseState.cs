a7ed34baaa4672b2870239317549076dde9fce0a
Woody <devkritt@gmail.com>
2026-05-23 21:55:24 +0700

    create BaseState and BaseStateController

---
+++ Assets/Scripts/EFTB/State/Base/BaseState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class BaseState
+{
+	public Dictionary<Type, Func<bool>> StateTransitionMap;
+	public bool IsStateActive { get; private set; }
+	protected BaseStateController StateController;
+
+
+	public BaseState(BaseStateController stateController)
+	{
+		StateController = stateController;
+		StateTransitionMap = new Dictionary<Type, Func<bool>>();
+	}
+
+	public virtual void InitializeState()
+    {
+
+    }
+
+	public virtual void Dispose()
+	{
+
+	}
+
+	public virtual void OnEnterState()
+	{
+
+	}
+
+	public virtual void UpdateLogic(float deltaTime)
+	{
+
+	}
+	public virtual void OnExitState()
+	{
+
+	}
+}
---
+++ Assets/Scripts/EFTB/State/Base/BaseStateController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+
+public abstract class BaseStateController
+{
+    public event Action<BaseState, BaseState> EventStateChanged;
+    public BaseState CurrentState { get; protected set; }
+    protected Dictionary<Type, BaseState> States { get; set; }
+    protected abstract Type DefaultTypeState { get; }
+
+    protected BaseStateController()
+    {
+        States = new Dictionary<Type, BaseState>();
+    }
+
+    public virtual void Initialize()
+    {
+        foreach(var state in States.Values)
+        {
+            state.Initialize();
+        }
+    }
+
+    public virtual void Dispose() 
+    {
+        if (CurrentState?.IsStateActive)
+        {
+            CurrentState.OnExitState();
+        }
+        CurrentState = null;
+
+        foreach(var state in States.Values)
+        {
+            state.Dispose();
+        }
+    }
+
+    public virtual void StartStateController(Type initialStateType = null)
+    {
+        var stateType = initialStateType ?? DefaultTypeState;
+        if (!States.TryGetValue(stateType, out var initialState))
+        {
+            return;
+        }
+
+        CurrentState = initialState;
+        CurrentState.OnEnterState();
+    }
+
+    public BaseState ChangeState(Type newState)
+    {
+        if (!IsValidToChangeState(newState))
+        {
+            return CurrentState;
+        }
+
+        var previousState = CurrentState;
+
+        CurrentState.OnExitState();
+        CurrentState = States[newState];
+        CurrentState.OnEnterState();
+
+        EventStateChanged?.Invoke(previousState, CurrentState);
+
+        return CurrentState;
+    }
+
+    public void UpdateLogic(float deltaTime)
+    {
+        if (CurrentState == null) return;
+
+        CurrentState.UpdateLogic(deltaTime);
+    }
+
+    public bool IsValidToChangeState(Type newState)
+    {
+        if (CurrentState.GetType() == newState)
+        {
+            return false;
+        }
+
+        if(!CurrentState.StateTransitionMap.TryGetValue(newState, out var changeCondition))
+        {
+            return false;
+        }
+
+        if(changeCondition != null && !changeCondition())
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
