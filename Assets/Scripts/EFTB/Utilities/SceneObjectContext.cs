4f34590d17ba02daaddf9b4b8db43c3c3a0396a9
Woody <devkritt@gmail.com>
2026-05-24 14:24:37 +0700

    create SceneObjectContext

---
+++ Assets/Scripts/EFTB/Utilities/SceneObjectContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.EFTB.Utilities
+{
+    public class SceneObjectContext : MonoBehaviour
+    {
+        public static SceneObjectContext Instance { get; private set; }
+
+        [SerializeField]
+        private MonoBehaviour[] sceneObjects;
+        private Dictionary<Type, MonoBehaviour> objects;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+                return;
+            }
+            Instance = this;
+        }
+
+        public void Initialize()
+        {
+            objects = new Dictionary<Type, MonoBehaviour>();
+
+            foreach (MonoBehaviour obj in sceneObjects)
+            {
+                Type key = obj.GetType();
+
+                if (!objects.ContainsKey(key))
+                {
+                    objects.Add(key, obj);
+                }
+                else
+                {
+                    Debug.LogWarning($"Duplicate type {key} found in SceneObjectContext. Only the first instance will be stored.");
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            objects = null;
+        }
+
+        public T Get<T> where T : class
+        {
+            objects.TryGetValue(typeof(T), out var result);
+            return result as T;
+        }
+    }
+}
