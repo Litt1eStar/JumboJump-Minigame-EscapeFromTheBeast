d0b8fe77d092b852ba7f931e9e4bba7c6c5af966
Woody <devkritt@gmail.com>
2026-05-25 12:13:00 +0700

    Create SightViewCone as the Sight View Cone Data

---
+++ Assets/Scripts/EFTB/Detection/SightViewCone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace Assets.Scripts.EFTB.Detection
+{
+    public class SightViewCone
+    {
+        public event Action EventTargetSpotted;
+        public event Action EventTargetLost;
+        private float fovAngle;
+        private float range;
+        public bool IsTargetVisible { get; private set; }
+        
+        public SightViewCone(float fovAngle, float range)
+        {
+            this.fovAngle = fovAngle;
+            this.range = range;
+        }
+
+        public void Initialize()
+        {
+            IsTargetVisible = false;
+        }
+        public void UpdateLogic(float deltaTime, Vector2 sightOrigin)
+        {
+            //Placeholder for target detection logic
+        }
+        public void Dispose()
+        {
+
+        }
+    }
+}
