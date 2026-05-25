743c71fcb96e32bc1131a97e0bdcdaf46637d603
Woody <devkritt@gmail.com>
2026-05-25 12:28:04 +0700

    Create CatSleepState

---
+++ Assets/Scripts/EFTB/State/Cat/SleepyCat/CatSleepState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.EFTB.State.Cat.SleepyCat
+{
+    public class CatSleepState : BaseState
+    {
+        public CatSleepState(BaseStateController stateController) : base(stateController)
+        {
+
+        }
+
+        public override void OnEnterState()
+        {
+            base.OnEnterState();
+        }
+
+        public override void OnExitState()
+        {
+            base.OnExitState();
+        }
+
+        public override void UpdateLogic(float deltaTime)
+        {
+            base.UpdateLogic(deltaTime);
+        }
+    }
+}
