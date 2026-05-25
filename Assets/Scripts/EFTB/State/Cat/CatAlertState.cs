2e877a665be0a586e79c30dfbea2c36543bd5b9c
Woody <devkritt@gmail.com>
2026-05-25 12:38:05 +0700

    Create CatAlertState

---
+++ Assets/Scripts/EFTB/State/Cat/CatAlertState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.EFTB.State.Cat
+{
+    public class CatAlertState : BaseState
+    {
+        public CatAlertState(BaseStateController stateController) : base(stateController)
+        {
+        }
+        public override void OnEnterState()
+        {
+            base.OnEnterState();
+        }
+
+        public override void OnExitState()
+        {
+            base.OnExitState();
+        }
+
+        public override void UpdateLogic(float deltaTime)
+        {
+            base.UpdateLogic(deltaTime);
+        }
+    }
+}
