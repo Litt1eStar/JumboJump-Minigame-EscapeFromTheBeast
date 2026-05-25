8b79e9ae6ed89b2b8b50d00e398fd268719a25f1
Woody <devkritt@gmail.com>
2026-05-25 12:24:47 +0700

    Create GICatSight for Cat Sight Instance

--- Assets/Scripts/EFTB/Detection/SightViewCone.cs
+++ Assets/Scripts/EFTB/Detection/SightViewCone.cs
@@ -1,33 +1,20 @@
 ﻿using System;
 using System.Numerics;
+using UnityEngine;
 
 namespace Assets.Scripts.EFTB.Detection
 {
     public class SightViewCone
     {
-        public event Action EventTargetSpotted;
-        public event Action EventTargetLost;
-        private float fovAngle;
-        private float range;
-        public bool IsTargetVisible { get; private set; }
-        
-        public SightViewCone(float fovAngle, float range)
-        {
-            this.fovAngle = fovAngle;
-            this.range = range;
-        }
+        public readonly LayerMask SIGHT_BLOCKER_LAYER_MASK;        
+        public float FovAngle { get; private set; }
+        public float Range {  get; private set; }
 
-        public void Initialize()
+        public SightViewCone(float fovAngle, float range, LayerMask sightBlockerLayerMask)
         {
-            IsTargetVisible = false;
-        }
-        public void UpdateLogic(float deltaTime, Vector2 sightOrigin)
-        {
-            //Placeholder for target detection logic
-        }
-        public void Dispose()
-        {
-
+            FovAngle = fovAngle;
+            Range = range;
+            SIGHT_BLOCKER_LAYER_MASK = sightBlockerLayerMask;
         }
     }
 }
---
+++ Assets/Scripts/EFTB/GI/GICatSight.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Scripts.EFTB.Detection;
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.EFTB.GI
+{
+    public class GICatSight : MonoBehaviour
+    {
+        [Header("Sight View Cone Configuration")]
+        [SerializeField]
+        private Transform sightOrigin;
+        
+        [SerializeField]
+        private float fovAngle;
+        
+        [SerializeField]
+        private float range;
+
+        [SerializeField]
+        private LayerMask sightBlockerLayerMask;
+
+        private SightViewCone sightViewConeData;
+        public void Initialize()
+        {
+            sightViewConeData = new SightViewCone(fovAngle, range, sightBlockerLayerMask);
+        }
+
+        public void Dispose()
+        {
+            sightViewConeData = null;
+        }
+
+        public void UpdateLogic(float deltaTime)
+        {
+            //Placeholder logic for Sight View Logic Calculation
+        }
+    }
+}
