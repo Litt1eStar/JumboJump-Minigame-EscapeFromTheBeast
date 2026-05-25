a79fd8e2c0660f70cc75824833690a0e6337be87
Woody <devkritt@gmail.com>
2026-05-25 12:41:14 +0700

    Create SleepyCatStateController

---
+++ Assets/Scripts/EFTB/State/Cat/SleepyCat/SleepyCatStateController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.EFTB.State.Cat.SleepyCat
+{
+    public class SleepyCatStateController : BaseStateController
+    {
+        protected override Type DefaultTypeState => typeof(CatSleepState);
+        public SleepyCatStateController()
+        {
+            States = new Dictionary<Type, BaseState>()
+            {
+                {typeof(CatSleepState), new CatSleepState(this) },
+                {typeof(CatAwakeState), new CatAwakeState(this) },
+                {typeof(CatAlertState), new CatAlertState(this) },
+            };
+        }
+    }
+}
