79e9d814df68b9b6bbe282457841be9c36df111d
Woody <devkritt@gmail.com>
2026-05-25 12:44:04 +0700

    Create CatManager

---
+++ Assets/Scripts/EFTB/Manager/CatManager.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.EFTB.State.Cat.SleepyCat;
+
+namespace Assets.Scripts.EFTB.Manager
+{
+    public class CatManager
+    {
+        private SleepyCatStateController sleepyCatStateController;
+
+        public void Intialize()
+        {
+            sleepyCatStateController = new SleepyCatStateController();
+        }
+
+        public void Dispose()
+        {
+            sleepyCatStateController = null;
+        }
+
+        public void UpdateLogic(float deltaTime)
+        {
+            sleepyCatStateController.UpdateLogic(deltaTime);
+        }
+    }
+}
