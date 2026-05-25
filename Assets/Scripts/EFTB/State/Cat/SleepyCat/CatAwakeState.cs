243ec901c7d07605df817fe0586fe07401aeb36c
Woody <devkritt@gmail.com>
2026-05-25 14:07:13 +0700

    Create CatAwakeLogic with Placeholder of isPlayerInSight

--- Assets/Scripts/EFTB/State/Cat/SleepyCat/CatAwakeState.cs
+++ Assets/Scripts/EFTB/State/Cat/SleepyCat/CatAwakeState.cs
@@ -2,6 +2,8 @@
 {
     public class CatAwakeState : BaseState
     {
+        private readonly float TIME_TO_CATCH = 5f;
+        private float countdownTimer = 0f;
         public CatAwakeState(BaseStateController stateController) : base(stateController)
         {
             StateTransitionMap.Add(typeof(CatSleepState), null);
@@ -11,6 +13,7 @@
         public override void OnEnterState()
         {
             base.OnEnterState();
+            countdownTimer = 0f;
         }
 
         public override void OnExitState()
@@ -20,7 +23,21 @@
 
         public override void UpdateLogic(float deltaTime)
         {
-            base.UpdateLogic(deltaTime);
+            countdownTimer += deltaTime;
+            
+            bool isPlayerInSight = true; // Replace with actual logic to check if player is in sight
+            
+            if(isPlayerInSight && countdownTimer < TIME_TO_CATCH)
+            {
+                // Transition to catch state
+                StateController.ChangeState(typeof(CatCatchState));
+            }
+
+            if(countdownTimer > TIME_TO_CATCH)
+            {
+                //Transition to sleep state
+                StateController.ChangeState(typeof(CatSleepState));
+            }
         }
     }
 }
