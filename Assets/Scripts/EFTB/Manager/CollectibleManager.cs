7b70faa15e43fa5a2286e9d01418b8a582f5ceae
Woody <devkritt@gmail.com>
2026-05-27 14:36:06 +0700

    ﻿Create CollectibleManger

---
+++ Assets/Scripts/EFTB/Manager/CollectibleManager.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts.EFTB.Utilities;
+using System;
+
+namespace Assets.Scripts.EFTB.Manager
+{
+    public class CollectibleManager
+    {
+        public event Action<int> EventTotalCoinValueChanged;
+        public int TotalCoinValue { get; private set; }
+        public void Initialize()
+        {
+            GameContext.Instance.Add(this);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public void AddCoin(int value)
+        {
+            TotalCoinValue += value;
+            EventTotalCoinValueChanged?.Invoke(TotalCoinValue); //Notify UI to update coin value
+        }
+    }
+}
