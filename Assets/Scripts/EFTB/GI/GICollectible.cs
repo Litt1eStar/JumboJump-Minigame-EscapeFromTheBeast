1b692da9560316b4599d05a469329ac96ff3fd86
Woody <devkritt@gmail.com>
2026-05-27 14:29:22 +0700

    Crete GICollectible, CollectibleSO

---
+++ Assets/Scripts/EFTB/GI/GICollectible.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.EFTB.GameData;
+using UnityEngine;
+
+namespace Assets.Scripts.EFTB.GI
+{
+    public class GICollectible : MonoBehaviour
+    {
+        [SerializeField]
+        private CollectibleSO collectibleData;
+
+        private SpriteRenderer spriteRenderer;
+        private bool isCollected = false;   
+
+        public void Initialize()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            
+        }
+    }
+}
