71c70ac1fd41db47ab15d5c36b693cc8809e1159
Woody <devkritt@gmail.com>
2026-05-28 17:44:53 +0700

    Create GameManger, GameStateController

---
+++ Assets/Scripts/EFTB/Manager/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.EFTB.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.EFTB.Manager
+{
+    public class GameManager
+    {
+        private GameStateController stateController;
+
+        public void Initialize()
+        {
+            stateController = new GameStateController();
+            stateController?.Initialize();
+            stateController?.StartStateController();
+        }
+
+        public void Dispose()
+        {
+            stateController?.Dispose();
+            stateController = null;
+        }
+    }
+}
