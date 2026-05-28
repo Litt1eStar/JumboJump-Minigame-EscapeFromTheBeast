a179427f6076e7f1bf90d7b5359dbe85ff2a6e0d
Woody <devkritt@gmail.com>
2026-05-28 17:44:53 +0700

    Create GameManger, GameStateController

---
+++ Assets/Scripts/EFTB/State/GameStateController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assets.Scripts.EFTB.State
+{
+    public class GameStateController : BaseStateController
+    {
+        protected override Type DefaultTypeState => throw new NotImplementedException();
+        public GameStateController()
+        {
+
+        }
+    }
+}
