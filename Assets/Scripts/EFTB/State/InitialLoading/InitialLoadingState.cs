13a10897c4ac427408d6882e5110249b2b3c82d5
Woody <devkritt@gmail.com>
2026-05-29 12:53:35 +0700

    Create InitialLoadingState, MainMenuState

---
+++ Assets/Scripts/EFTB/State/InitialLoading/InitialLoadingState.cs
@@ -0,0 +1,24 @@
+﻿namespace JumboJump.Assets.Scripts.EFTB.State.InitialLoading
+{
+    public class InitialLoadingState : BaseState
+    {
+        public InitialLoadingState(BaseStateController stateController) : base(stateController)
+        {
+        }
+
+        public override void OnEnterState()
+        {
+            base.OnEnterState();
+        }
+
+        public override void OnExitState()
+        {
+            base.OnExitState();
+        }
+
+        public override void UpdateLogic(float deltaTime)
+        {
+            base.UpdateLogic(deltaTime);
+        }
+    }
+}
