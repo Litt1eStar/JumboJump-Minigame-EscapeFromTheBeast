1bfbde2a6348eb7e408664f4d5e5eb40a3df8059
Woody <devkritt@gmail.com>
2026-05-30 14:27:08 +0700

    Move coin event subscription from CollectibleVisualizer to GameplayState

--- Assets/Scripts/EFTB/State/Gameplay/GameplayState.cs
+++ Assets/Scripts/EFTB/State/Gameplay/GameplayState.cs
@@ -1,10 +1,13 @@
-﻿using JumboJumps.EFTB.Visualizer;
+﻿using JumboJumps.EFTB.Manager;
+using JumboJumps.EFTB.Utilities;
+using JumboJumps.EFTB.Visualizer;
 
 namespace JumboJumps.EFTB.State.Gameplay
 {
     public class GameplayState : BaseState
     {
         private CollectibleVisualizer collectibleVisualizer;
+        private CollectibleManager collectibleManager;
 
         public GameplayState(BaseStateController stateController) : base(stateController)
         {
@@ -15,13 +18,26 @@ namespace JumboJumps.EFTB.State.Gameplay
         {
             base.OnEnterState();
 
+            collectibleManager = GameContext.Instance.Get<CollectibleManager>();
+            if(collectibleManager == null)
+            {
+                DebugLogHelper.LogError("GameplayState: CollectibleManager not found in GameContext.");
+             }
+
             collectibleVisualizer = new CollectibleVisualizer();
             collectibleVisualizer.Initialize();
+
+            collectibleManager.EventTotalCoinValueChanged += collectibleVisualizer.UpdateCoinChanged;
         }
 
         public override void OnExitState()
         {
             base.OnExitState();
+
+            if(collectibleManager != null)
+            {
+                collectibleManager.EventTotalCoinValueChanged -= collectibleVisualizer.UpdateCoinChanged;
+            }
 
             collectibleVisualizer?.Dispose();
             collectibleVisualizer = null;
--- Assets/Scripts/EFTB/Visualizer/CollectibleVisualizer.cs
+++ Assets/Scripts/EFTB/Visualizer/CollectibleVisualizer.cs
@@ -6,23 +6,17 @@ namespace JumboJumps.EFTB.Visualizer
 {
     public class CollectibleVisualizer
     {
-        private CollectibleManager collectibleManager;
         public void Initialize()
         {
-            collectibleManager = GameContext.Instance.Get<CollectibleManager>();
-            if(collectibleManager == null)
-            {
-                DebugLogHelper.LogWarning("CollectibleVisualizer: CollectibleManager not found in GameContext. Visualization will not work.");
-            }
-            collectibleManager.EventTotalCoinValueChanged += OnTotalCoinValueChanged;
+          
         }
 
         public void Dispose()
         {
-            collectibleManager.EventTotalCoinValueChanged -= OnTotalCoinValueChanged;
+            
         }
 
-        public void OnTotalCoinValueChanged(int newTotalValue)
+        public void UpdateCoinChanged(int newTotalValue)
         {
             DebugLogHelper.Log($"CollectibleVisualizer: Total Coin Value Updated to {newTotalValue}");
             //Update UI 
