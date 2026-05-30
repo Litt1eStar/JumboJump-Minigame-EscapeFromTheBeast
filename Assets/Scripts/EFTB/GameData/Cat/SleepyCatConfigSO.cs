aadfae9ac4ffbaa306876099af9448626b40f9e9
Woody <devkritt@gmail.com>
2026-05-30 13:13:54 +0700

    Change parameter name of private variable and remove empty line in SleepyCatConfigSO

--- Assets/Scripts/EFTB/GameData/Cat/SleepyCatConfigSO.cs
+++ Assets/Scripts/EFTB/GameData/Cat/SleepyCatConfigSO.cs
@@ -9,7 +9,6 @@ namespace JumboJumps.EFTB.GameData.Cat
     [CreateAssetMenu(fileName = "SleepyCatConfigSO", menuName = "Scriptable Objects/SleepyCatConfigSO")]
     public class SleepyCatConfigSO : BaseCatConfigSO
     {
-
         [SerializeField]
         private float timeTillAwake;
         
@@ -18,14 +17,12 @@ namespace JumboJumps.EFTB.GameData.Cat
         
         [SerializeField]
         private float timeToCatch;
-
         public float TimeTillAwake => timeTillAwake;
         public float TimeToAlert => timeToAlert;
         public float TimeToCatch => timeToCatch;
-
-        public override ICatStateController BuildStateController(GICat sight, Transform transform, UICatStateLabel label)
+        public override ICatStateController BuildStateController(GICat giCat, Transform transform, UICatStateLabel label)
         {
-            SleepyCatStateController controller = new SleepyCatStateController(this, sight, label, transform);
+            SleepyCatStateController controller = new SleepyCatStateController(this, giCat, label, transform);
             return controller;
         }
     }
