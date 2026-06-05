5a39e2357f1063d61ef1722d89c16524b7810dcb
Woody <devkritt@gmail.com>
2026-06-05 10:26:52 +0700

    Change responsibility to Create and Manage GameVisualizer to GameStateController and Extract Event Subscribtion into method Subscribe and Unsubscribe

--- Assets/Scripts/EFTB/Manager/GameManager.cs
+++ Assets/Scripts/EFTB/Manager/GameManager.cs
@@ -1,44 +1,26 @@
-﻿using JumboJumps.EFTB.Visualizer;
-using JumboJumps.EFTB.State;
+﻿using JumboJumps.EFTB.State;
 
 namespace JumboJumps.EFTB.Manager
 {
     public class GameManager
     {
         private GameStateController stateController;
-        private GameVisualizer gameVisualizer;
 
         public void Initialize()
         {
-            gameVisualizer = new GameVisualizer();
-            gameVisualizer.Initialize();
-
             stateController = new GameStateController();
-            stateController.EventStateChanged += OnGameStateChanged;
             stateController.Initialize();
         }
 
         public void Dispose()
         {
-            if(stateController != null)
-            {
-                stateController.EventStateChanged -= OnGameStateChanged;    
-                stateController.Dispose();
-                stateController = null;
-            }
-
-            gameVisualizer?.Dispose();
-            gameVisualizer = null;
+            stateController?.Dispose();
+            stateController = null;   
         }
 
         public void UpdateLogic(float deltaTime)
         {
             stateController?.UpdateLogic(deltaTime);
-        }
-
-        public void OnGameStateChanged(BaseState prev, BaseState next)
-        {
-            gameVisualizer.UpdateOuterStateLabel(next.GetType().Name);
         }
     }
 }
--- Assets/Scripts/EFTB/State/GameStateController.cs
+++ Assets/Scripts/EFTB/State/GameStateController.cs
@@ -3,12 +3,14 @@ using JumboJumps.EFTB.State.Gameplay;
 using JumboJumps.EFTB.State.MainMenu;
 using System;
 using System.Collections.Generic;
+using JumboJumps.EFTB.Visualizer;
 
 namespace JumboJumps.EFTB.State
 {
     public class GameStateController : BaseStateController
     {
         protected override Type DefaultTypeState => typeof(InitialLoadingState);
+        public GameVisualizer Visualizer { get; private set; }
 
         public GameStateController()
         {
@@ -18,6 +20,36 @@ namespace JumboJumps.EFTB.State
                 {typeof(GameplayState), new GameplayState(this) },
                 {typeof(MainMenuState), new MainMenuState(this) }
             };
+
+            Visualizer = new GameVisualizer();
+            Visualizer.Initialize();
+
+            Subscribe();
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            Unsubscribe();
+
+            Visualizer?.Dispose();
+            Visualizer = null;
+        }
+
+        private void Subscribe()
+        {
+            EventStateChanged += OnGameStateChanged;
+        }
+
+        private void Unsubscribe()
+        {
+            EventStateChanged -= OnGameStateChanged;
+        }
+
+        public void OnGameStateChanged(BaseState prev, BaseState next)
+        {
+            Visualizer.UpdateOuterStateLabel(next.GetType().Name);
         }
     }
 }
