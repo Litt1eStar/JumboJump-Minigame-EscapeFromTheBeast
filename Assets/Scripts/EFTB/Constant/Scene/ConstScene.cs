dda9cd6f34ea25e286ccd682019a23bbd349b9ee
Woody <devkritt@gmail.com>
2026-06-05 13:18:55 +0700

    Create ConstScene

---
+++ Assets/Scripts/EFTB/Constant/Scene/ConstScene.cs
@@ -0,0 +1,8 @@
+﻿namespace JumboJump.Assets.Scripts.EFTB.Constant.Scene
+{
+    public static class ConstScene
+    {
+        public const string MAIN_MENU = "MainMenu";
+        public const string GAMEPLAY = "Gameplay";
+    }
+}
