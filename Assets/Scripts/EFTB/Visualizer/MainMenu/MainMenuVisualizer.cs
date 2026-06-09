f9cef83427151e839b8b11dd0dce3c40e7e58ee8
Woody <devkritt@gmail.com>
2026-06-09 14:02:19 +0700

    Create MainMenuVisualizer Logic

--- Assets/Scripts/EFTB/Visualizer/MainMenu/MainMenuVisualizer.cs
+++ Assets/Scripts/EFTB/Visualizer/MainMenu/MainMenuVisualizer.cs
@@ -1,7 +1,31 @@
-﻿namespace JumboJumps.EFTB.Visualizer.MainMenu
+﻿using JumboJumps.EFTB.UI.MainMenu;
+using JumboJumps.EFTB.Utilities;
+using System;
+
+namespace JumboJumps.EFTB.Visualizer.MainMenu
 {
     public class MainMenuVisualizer
     {
+        private UIMainMenuCanvas uiMainMenuCanvas;
+        
+        public void Initialize()
+        {
+            uiMainMenuCanvas = SceneObjectContext.Instance.Get<UIMainMenuCanvas>();
 
+            if(uiMainMenuCanvas == null)
+            {
+                DebugLogHelper.LogError("Failed to initialize MainMenuVisualizer: UIMainMenuCanvas not found in scene.");
+            }
+        }
+
+        public void Dispose() 
+        {
+            uiMainMenuCanvas = null;
+        }
+
+        public void Subscribe(Action playBtnCallback, Action exitBtnCallback)
+        {
+            uiMainMenuCanvas?.Subscribe(playBtnCallback, exitBtnCallback);
+        }
     }
 }
