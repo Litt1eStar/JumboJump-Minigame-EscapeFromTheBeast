eeaece6b36c575998aa47948ac087350bd311dfd
Woody <devkritt@gmail.com>
2026-06-09 14:46:48 +0700

    Create UIGameplayPanel Logic

--- Assets/Scripts/EFTB/UI/Gameplay/UIGameplayPanel.cs
+++ Assets/Scripts/EFTB/UI/Gameplay/UIGameplayPanel.cs
@@ -1,6 +1,47 @@
-﻿namespace JumboJumps.EFTB.UI.Gameplay
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace JumboJumps.EFTB.UI.Gameplay
 {
     public class UIGameplayPanel : UIBasePanel
     {
+        [SerializeField]
+        private Button pauseButton;
+
+        [SerializeField]
+        private TextMeshProUGUI coinCounterLabel;
+
+        public void Initialize()
+        {
+            if (coinCounterLabel != null)
+            {
+                coinCounterLabel.text = "Coins: 0";
+            }
+        }
+
+        public override void Show()
+        {
+            base.Show();
+        }
+
+        public override void Hide()
+        {
+            base.Hide();
+        }
+
+        public void Subscribe(Action pauseBtnCallback)
+        {
+            if (pauseButton != null)
+            {
+                pauseButton.onClick.AddListener(() => pauseBtnCallback());
+            }
+        }
+
+        public void SetCoinCounterLabel(int value)
+        {
+            coinCounterLabel.text = $"Coins: {value}";
+        }
     }
 }
