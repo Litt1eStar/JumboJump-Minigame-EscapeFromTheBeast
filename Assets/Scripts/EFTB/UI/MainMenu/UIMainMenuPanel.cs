9d2fddc72b6d74ad048d86d85f5494b19be85be2
Woody <devkritt@gmail.com>
2026-06-09 13:57:41 +0700

    Create UIMainMenuPanel Logic

--- Assets/Scripts/EFTB/UI/MainMenu/UIMainMenuPanel.cs
+++ Assets/Scripts/EFTB/UI/MainMenu/UIMainMenuPanel.cs
@@ -1,7 +1,17 @@
-﻿namespace JumboJumps.EFTB.UI.MainMenu
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace JumboJumps.EFTB.UI.MainMenu
 {
     public class UIMainMenuPanel : UIBasePanel
     {
+        [SerializeField]
+        private Button playButton;
+
+        [SerializeField]
+        private Button exitButton;
+
         public override void Show()
         {
             base.Show();
@@ -10,6 +20,35 @@
         public override void Hide()
         {
             base.Hide();
+        }
+
+        public void Subscribe(Action playBtnCallback, Action exitBtnCallback)
+        {
+            if (playButton != null)
+            {
+                playButton.onClick.AddListener(() => playBtnCallback());
+            }
+
+            if (exitButton != null)
+            {
+                exitButton.onClick.AddListener(() => exitBtnCallback());
+            }
+        }
+
+        public void OnClickPlayButton()
+        {
+            if (playButton != null) 
+            {
+                playButton.onClick.Invoke();
+            }
+        }
+
+        public void OnClickExitButton()
+        {
+            if(exitButton != null)
+            {
+                exitButton.onClick.Invoke();
+            }
         }
     }
 }
