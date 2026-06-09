b42c662882bfb7fcac4f24926bdf1c9efbe1cc51
Woody <devkritt@gmail.com>
2026-06-09 13:13:17 +0700

    Create logic for InitialLoadingVisualizer

--- Assets/Scripts/EFTB/Visualizer/InitialLoading/InitialLoadingVisualizer.cs
+++ Assets/Scripts/EFTB/Visualizer/InitialLoading/InitialLoadingVisualizer.cs
@@ -1,7 +1,35 @@
-﻿namespace JumboJumps.EFTB.Visualizer.InitialLoading
+﻿using JumboJumps.EFTB.UI.InitialLoading;
+using JumboJumps.EFTB.Utilities;
+
+namespace JumboJumps.EFTB.Visualizer.InitialLoading
 {
     public class InitialLoadingVisualizer
     {
+        private UILoadingCanvas uiLoadingCanvas;
 
+        public void Initialize()
+        {
+            uiLoadingCanvas = SceneObjectContext.Instance.Get<UILoadingCanvas>();
+        }
+
+        public void Dispose()
+        {
+            uiLoadingCanvas = null;
+        }
+
+        public void SetProgress(float value)
+        {
+            uiLoadingCanvas?.SetProgress(value);
+        }
+
+        public void Show()
+        {
+            uiLoadingCanvas?.Show();
+        }
+
+        public void Hide()
+        {
+            uiLoadingCanvas?.Hide();
+        }
     }
 }
