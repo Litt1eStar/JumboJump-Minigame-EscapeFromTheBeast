e273ccc089460ae23ff016b4901e07156f49ff38
Woody <devkritt@gmail.com>
2026-06-09 13:59:40 +0700

    Create UIMainMenuCanvas logic

--- Assets/Scripts/EFTB/UI/MainMenu/UIMainMenuCanvas.cs
+++ Assets/Scripts/EFTB/UI/MainMenu/UIMainMenuCanvas.cs
@@ -1,9 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace JumboJumps.EFTB.UI.MainMenu
 {
     public class UIMainMenuCanvas : MonoBehaviour
     {
+        [SerializeField]
+        private UIMainMenuPanel uiMainMenuPanel;
 
+        public void Show()
+        {
+            uiMainMenuPanel?.Show();
+        }
+
+        public void Hide()
+        {
+            uiMainMenuPanel?.Hide();
+        }
+        
+        public void Subscribe(Action playBtnCallback, Action exitBtnCallback)
+        {
+            uiMainMenuPanel?.Subscribe(playBtnCallback, exitBtnCallback);
+        }
     }
 }
