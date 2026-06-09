dc22a97e687b40e754f8413211456aeaf45371e7
Woody <devkritt@gmail.com>
2026-06-09 13:05:36 +0700

    Create UILoadingPanel logic

--- Assets/Scripts/EFTB/UI/InitialLoading/UILoadingPanel.cs
+++ Assets/Scripts/EFTB/UI/InitialLoading/UILoadingPanel.cs
@@ -1,6 +1,29 @@
-﻿namespace JumboJumps.EFTB.UI.InitialLoading
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace JumboJumps.EFTB.UI.InitialLoading
 {
     public class UILoadingPanel : UIBasePanel
     {
+        [SerializeField]
+        private Slider loadingProgressBar;
+
+        public override void Show()
+        {
+            base.Show();
+        }
+
+        public override void Hide()
+        {
+            base.Hide();
+        }
+
+        public void SetProgress(float value)
+        {
+            if(loadingProgressBar != null)
+            {
+                loadingProgressBar.value = value;
+            }
+        }
     }
 }
