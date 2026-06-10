60d4118e88d6bd9019128141c4aaa3d9d96b1299
Woody <devkritt@gmail.com>
2026-06-10 11:30:29 +0700

    Edit Scene Layout and Create GameStatus for tracking game status

--- Assets/Scripts/EFTB/Manager/GameplayStateManager.cs
+++ Assets/Scripts/EFTB/Manager/GameplayStateManager.cs
@@ -4,12 +4,20 @@ using JumboJumps.EFTB.Utilities;
 
 namespace JumboJumps.EFTB.Manager
 {
+    public enum GameStatus 
+    { 
+        Default,
+        Winning,
+        Losing
+    }
+
     public class GameplayStateManager
     {
         private GameplayStateController stateController;
         private GameVisualizer gameVisualizer;
 
         public GameplayStateController StateController => stateController;
+        public GameStatus GameStatus { get; private set; } = GameStatus.Default;
 
         public void Initialize(GameplayController gameplayController)
         {
@@ -36,6 +44,20 @@ namespace JumboJumps.EFTB.Manager
         public void UpdateLogic(float deltaTime)
         {
             stateController?.UpdateLogic(deltaTime);
+        }
+
+        public void WinGame()
+        {
+            if(GameStatus != GameStatus.Default) return;
+
+            GameStatus = GameStatus.Winning;
+        }
+
+        public void LoseGame()
+        {
+            if(GameStatus != GameStatus.Default) return;
+
+            GameStatus = GameStatus.Losing;
         }
     }
 }
