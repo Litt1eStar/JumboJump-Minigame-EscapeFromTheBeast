62eec84ec9d5c288c2e4f6e9f05c3019b2c115d8
Woody <devkritt@gmail.com>
2026-06-17 10:09:28 +0700

    Remove Unity Scripting Define Symbol in Input2DManager

--- Assets/Scripts/EFTB/Manager/Input2DManager.cs
+++ Assets/Scripts/EFTB/Manager/Input2DManager.cs
@@ -5,12 +5,9 @@ namespace JumboJumps.EFTB.Manager
 {
     public class Input2DManager : MonoBehaviour
     {
-        [SerializeField]
-        private KeyCode changeStateKey = KeyCode.Space; // For testing purpose, can be removed later
-
         public float XInput { get; private set; }
-        public float YInput { get; private set; }  
-        
+        public float YInput { get; private set; }
+
         public void Initialize()
         {
             GameContext.Instance.Add(this);
@@ -31,15 +28,5 @@ namespace JumboJumps.EFTB.Manager
             XInput = Input.GetAxis("Horizontal");
             YInput = Input.GetAxis("Vertical");
         }
-
-        /// <summary>
-        /// For testing purpose
-        /// </summary>
-#if UNITY_EDITOR
-        public bool IsChangeState()
-        {
-            return Input.GetKeyDown(changeStateKey);
-        }
-#endif
     }
 }
