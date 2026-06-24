e393889ff217ca365d6e225c15bf09027d8f3747
Woody <devkritt@gmail.com>
2026-06-24 15:23:59 +0700

    Create PoolableObject script that will use to attach to prefab that will working with Object Pool

---
+++ Assets/Scripts/EFTB/Utilities/PoolableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace JumboJump.EFTB.Utilities
+{
+    public class PoolableObject : MonoBehaviour
+    {
+        /// <summary>
+        /// Attached this script to prefab that working with Object Pool
+        /// </summary>
+
+        public string PoolKey { get; set; }
+        
+        public virtual void OnSpawn()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public virtual void OnRecycle()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
