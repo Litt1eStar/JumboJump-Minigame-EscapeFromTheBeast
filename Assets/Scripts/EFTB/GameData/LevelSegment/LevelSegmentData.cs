67b290aa3887ebdb9e204011cf5c9a4f4280c314
Woody <devkritt@gmail.com>
2026-06-24 15:12:42 +0700

    Create LevelSegmentData and define 2 new struct for LaneObject and LaneEvent

---
+++ Assets/Scripts/EFTB/GameData/LevelSegment/LevelSegmentData.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace JumboJump.EFTB.GameData.LevelSegment
+{
+    [CreateAssetMenu(fileName = "LevelSegmentData", menuName = "EFTB/LevelSegmentData")]
+    public class LevelSegmentData : ScriptableObject
+    {
+        [Header("Segment Settings")]
+        public GameObject segmentPrefab;
+        public float segmentHeight = 20f;
+
+        [Header("Object Spawn Layout")]
+        public List<LaneObjectSpawnData> prePlacedObjectsData;
+        public List<LaneEventSpawnData> spawnEventData;
+    }
+
+    [Serializable]
+    public struct LaneObjectSpawnData
+    {
+        /// <summary>
+        /// laneIndex : index of lane (0 - 4)
+        /// </summary>
+        public int laneIndex;
+
+        /// <summary>
+        /// yOffset : Vertical offset from the bottom of segment
+        /// </summary>
+        public float yOffset;
+
+        /// <summary>
+        /// prefab : HidableObject or coin Prefab
+        /// </summary>
+        public GameObject prefab;
+    }
+
+    [Serializable]
+    public struct LaneEventSpawnData
+    {
+        /// <summary>
+        /// targetLaneIndex : Index of Lane that Event will moving through
+        /// </summary>
+        public int targetLaneIndex;
+
+        /// <summary>
+        /// triggerYOffset : Distance from start of segment to trigger the event
+        /// </summary>
+        public float triggerYOffset;
+
+        public GameObject movingObstaclePrefab;
+        public float speed;
+    }
+}
