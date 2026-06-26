c6d996dd9e42cf2a1b3f2a3fad0aeb92f461536b
Woody <devkritt@gmail.com>
2026-06-26 13:29:48 +0700

    Create placeholder function to define flow in GameDataManager

--- Assets/Scripts/EFTB/Manager/GameDataManager.cs
+++ Assets/Scripts/EFTB/Manager/GameDataManager.cs
@@ -5,14 +5,34 @@ namespace JumboJumps.EFTB.Manager
 {
     public class GameDataManager : MonoBehaviour
     {
+        [SerializeField]
+        private TextAsset localLevelSegmentData;
+
         public void Initialize()
         {
+            LoadGameData();
+
             GameContext.Instance.Add(this);
         }
 
         public void Dispose()
         {
             GameContext.Instance.Remove(this);
+        }
+
+        public void LoadGameData()
+        {
+            OnGameDataLoaded(localLevelSegmentData.text);
+        }
+
+        public void OnGameDataLoaded(string jsonText)
+        {
+            ParseAndApplyGameDataFromJson(jsonText);
+        }
+
+        public void ParseAndApplyGameDataFromJson(string jsonText)
+        {
+
         }
     }
 }
