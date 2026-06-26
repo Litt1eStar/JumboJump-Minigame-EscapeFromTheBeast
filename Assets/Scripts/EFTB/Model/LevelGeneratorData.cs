48d2ea8eb54ecc500e8484e926b2ce4b4b6818fc
Woody <devkritt@gmail.com>
2026-06-26 13:39:16 +0700

    Create LevelGeneratorData

---
+++ Assets/Scripts/EFTB/Model/LevelGeneratorData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JumboJump.Assets.Scripts.EFTB.Model
+{
+    public class LevelGeneratorData
+    {
+        [Serializable]
+        public class LevelGeneratorConfigData
+        {
+            List<LevelSegmentData> segmentTemplate;
+            public float laneXPositions;
+            public int maxSegmentAmount;
+            public float segmentHeight;
+            public float segmentRecycleTriggerOffset;
+            public float mediumDifficultyDistance;
+            public float hardDifficultyDistance;
+        }
+
+        [Serializable]
+        public class LevelSegmentData
+        {
+            public string segmentPrefabName;
+            public string segmentHeight;
+            public string difficulty;
+            public List<LaneObjectData> prePlacedObjectDatas;
+            public List<LaneEventData> laneEventDatas;
+        }
+
+        [Serializable]
+        public class LaneObjectData
+        {
+            public int laneIndex;
+            public float yOffset;
+            public string prefabName;
+        }
+
+        [Serializable]
+        public class LaneEventData
+        {
+            public int laneIndex;
+            public float yOffset;
+            public float speed;
+            public string prefabName;
+        }
+    }
+}
