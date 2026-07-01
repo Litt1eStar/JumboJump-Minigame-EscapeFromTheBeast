7e215c8f53a8d956ff508902dc40b395ddd0c688
Woody <devkritt@gmail.com>
2026-07-01 10:05:13 +0700

    Refactor GetPrefab in PrefabRegistrySO to be O(1) instead of O(n) by change from Searching List to Lookup the Dictionary

--- Assets/Scripts/EFTB/GameData/PrefabRegistrySO.cs
+++ Assets/Scripts/EFTB/GameData/PrefabRegistrySO.cs
@@ -6,18 +6,44 @@ namespace JumboJumps.EFTB.GameData
     public class PrefabRegistrySO : ScriptableObject
     {
         public List<GameObject> registry = new();
+
+        private Dictionary<string, GameObject> prefabCache;
+        public Dictionary<string, GameObject> PrefabCache
+        {
+            get
+            {
+                if (prefabCache == null)
+                {
+                    prefabCache = new Dictionary<string, GameObject>();
+                    
+                    foreach (var prefab in registry)
+                    {
+                        if (prefab != null && !prefabCache.ContainsKey(prefab.name))
+                        {
+                            prefabCache.Add(prefab.name, prefab);
+                        }
+                    }
+                }
+                return prefabCache;
+            }
+        }
+
         public GameObject GetPrefab(string prefabName)
         {
             if (string.IsNullOrEmpty(prefabName)) return null;
 
-            var prefab = registry.Find(e => e.name == prefabName);
-
-            if (prefab == null)
+            if(PrefabCache.TryGetValue(prefabName, out GameObject prefab))
             {
-                Debug.LogWarning($"[{GetType().Name}] Prefab not found for name: {prefabName}");
+                return prefab;
             }
 
-            return prefab;
+            Debug.LogWarning($"[{GetType().Name}] Prefab not found for name: {prefabName}");
+            return null;
+        }
+
+        private void OnValidate()
+        {
+            prefabCache = null;
         }
     }
 }
