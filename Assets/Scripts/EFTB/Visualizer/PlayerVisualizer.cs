a12c57a3e95e5627d12a8b22c2cd9dc7684fad2e
Woody <devkritt@gmail.com>
2026-08-04 11:09:24 +0700

    Resovle PR [ESTB-032]

--- Assets/Scripts/EFTB/Visualizer/PlayerVisualizer.cs
+++ Assets/Scripts/EFTB/Visualizer/PlayerVisualizer.cs
@@ -1,5 +1,6 @@
 using JumboJumps.EFTB.GI;
 using JumboJumps.EFTB.Utilities;
+using System;
 using UnityEngine;
 
 namespace JumboJumps.EFTB.Visualizer
@@ -43,7 +44,7 @@ namespace JumboJumps.EFTB.Visualizer
             giPlayer?.SetXPosition(x);
         }
 
-        public void ShowPounceWarning(float duration, System.Action onComplete)
+        public void ShowPounceWarning(float duration, Action onComplete)
         {
             giPlayer?.ShowPounceWarning(duration, onComplete);
         }
