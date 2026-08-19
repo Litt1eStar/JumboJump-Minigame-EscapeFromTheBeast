d674e13c97e86237a6b2483fc841417ae38a1643
Woody <devkritt@gmail.com>
2026-08-19 10:51:10 +0700

    #

--- Assets/Scripts/EFTB/Model/Obstacle/FurniturePlacementModel.cs
+++ Assets/Scripts/EFTB/Model/Obstacle/FurniturePlacementModel.cs
@@ -6,19 +6,6 @@ using UnityEngine;
 
 namespace JumboJumps.EFTB.Model.Obstacle
 {
-    public class FurnitureBlockData
-    {
-        public int LaneIndex { get; }
-        public float YOffset { get; }
-        public string PrefabName { get; }
-
-        public FurnitureBlockData(int laneIndex, float yOffset, string prefabName)
-        {
-            LaneIndex = laneIndex;
-            YOffset = yOffset;
-            PrefabName = prefabName;
-        }
-    }
 
     public class FurniturePlacementModel
     {
