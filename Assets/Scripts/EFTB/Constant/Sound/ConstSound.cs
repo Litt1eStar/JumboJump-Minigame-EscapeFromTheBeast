de7d24a0793fbb2d8fc40622ace4b77145e779ca
Woody <devkritt@gmail.com>
2026-08-19 23:17:29 +0700

    Import sound asset

--- Assets/Scripts/EFTB/Constant/Sound/ConstSound.cs
+++ Assets/Scripts/EFTB/Constant/Sound/ConstSound.cs
@@ -4,15 +4,9 @@ namespace JumboJumps.EFTB.Constant.Sound
     {
         public static class Keys
         {
-            public static class Player
-            {
-                public const string MOVE = "SFX_Player_Move";
-            }
-
             public static class Cat
             {
-                public const string POUNCE_WARNING = "SFX_Cat_PounceWarning";
-                public const string SMASH = "SFX_Cat_Smash";
+                public const string MEOW_SAD = "SFX_Cat_MeowSad";
             }
 
             public static class Collectible
@@ -24,11 +18,12 @@ namespace JumboJumps.EFTB.Constant.Sound
             {
                 public const string BUTTON_CLICK = "SFX_UI_ButtonClick";
                 public const string GAME_OVER = "SFX_UI_GameOver";
+                public const string READY = "SFX_UI_Ready";
+                public const string GO = "SFX_UI_GO";
             }
 
             public static class BGM
             {
-                public const string MAIN_MENU = "BGM_MainMenu";
                 public const string GAMEPLAY = "BGM_Gameplay";
             }
         }
--- Assets/Scripts/EFTB/Sound/EFTBSound.cs
+++ Assets/Scripts/EFTB/Sound/EFTBSound.cs
@@ -34,17 +34,9 @@ namespace JumboJumps.EFTB.Sound
             }
         }
 
-        public static void PlayPlayerMove() => PlaySFX(ConstSound.Keys.Player.MOVE);
-
-        public static void PlayCatPounceWarning() => PlaySFX(ConstSound.Keys.Cat.POUNCE_WARNING);
-        public static void PlayCatSmash() => PlaySFX(ConstSound.Keys.Cat.SMASH);
-
         public static void PlayTreatCollected() => PlaySFX(ConstSound.Keys.Collectible.TREAT_COLLECT);
-
         public static void PlayUIClick() => PlaySFX(ConstSound.Keys.UI.BUTTON_CLICK);
         public static void PlayGameOver() => PlaySFX(ConstSound.Keys.UI.GAME_OVER);
-
         public static void PlayGameplayBGM() => PlayBGM(ConstSound.Keys.BGM.GAMEPLAY);
-        public static void PlayMainMenuBGM() => PlayBGM(ConstSound.Keys.BGM.MAIN_MENU);
     }
 }
